7b1875ef907377faa2b11c8b68eaa472a83d4823
Seamus Murphy <seamusrobertmurphy@gmail.com>
2022-05-10 15:03:49 -0700

    loca config

--- apps/home.py
+++ apps/home.py
@@ -1,19 +1,36 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import geemap
+import ee
+import ipyleaflet
+import os
+
 
 
 def app():
-    st.title("Home")
+    st.title("Active Fires")
 
     st.markdown(
         """
-    A [streamlit](https://streamlit.io) app template for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
-    To create a direct link to a pre-selected menu, add `?page=<app name>` to the URL, e.g., `?page=upload`.
-    https://share.streamlit.io/giswqs/streamlit-template?page=upload
+    We acknowledge the use of data and/or imagery from NASA's Fire 
+    Information for Resource Management System (FIRMS) 
+    (https://earthdata.nasa.gov/firms), part of NASA's 
+    Earth Observing System Data and Information System (EOSDIS).
 
+    FIRMS distributes near real-time and standard fire products from: 
+    Moderate Resolution Imaging Spectroradiometer (MODIS) 
+    from the Terra and Aqua platforms, and Visible Infrared Imaging 
+    Radiometer Suite (VIIRS) (375m) from the Suomi NPP and 
+    NOAA-20 platforms.  
+    
     """
     )
 
+
     m = leafmap.Map(locate_control=True)
-    m.add_basemap("ROADMAP")
+    m.add_basemap("SATELLITE")
     m.to_streamlit(height=700)
+    dataset_inz = ee.ImageCollection('FIRMS')
+    image2 = dataset_inz.first()
+    m.addLayer(image2, {}, 'Active Fires')
+    m
