a1a2d83a4552fdf8666bc501990db16029c05e4b
Seamus Murphy <seamusrobertmurphy@gmail.com>
2022-05-10 15:12:10 -0700

    local config

--- apps/home.py
+++ apps/home.py
@@ -27,7 +27,7 @@ def app():
     )
 
 
-    m = leafmap.Map(center=(52.5, -119), zoom=4)
+    m = leafmap.Map(locate_control=TRUE)
     m.add_basemap("SATELLITE")
     m.to_streamlit(height=700)
     dataset_inz = ee.ImageCollection('FIRMS')
