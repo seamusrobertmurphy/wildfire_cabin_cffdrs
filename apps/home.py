e549a661be791ee6b270105c7a9a8780b1700721
Seamus Murphy <seamusrobertmurphy@gmail.com>
2022-05-10 17:31:39 -0700

    local config

--- apps/home.py
+++ apps/home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import geemap.foliumap as geemap
 
 
 def app():
@@ -11,6 +12,13 @@ def app():
     """
     )
 
-    m = leafmap.Map(locate_control=True)
-    m.add_basemap("ROADMAP")
+    m = geemap.Map(locate_control=True)
+    m.add_basemap("TERRAIN")
+    dem = ee.Image("USGS/SRTMGL1_003")
+    vis_params = {
+    "min": 0,
+    "max": 4000,
+    "palette": ["006633", "E5FFCC", "662A00", "D8D8D8", "F5F5F5"],
+}
+    m.addLayer(dem, vis_params, "SRTM DEM", True, 0.5)
     m.to_streamlit(height=700)
