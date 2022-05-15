17b0dc03618f07befe50c01a384314960e1e1edc
Seamus Murphy <seamusrobertmurphy@gmail.com>
2022-05-15 16:18:37 -0700

    active fires pip

--- apps/home.py
+++ apps/home.py
@@ -1,16 +1,12 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-import geemap
-import ee
-import ipyleaflet
-import os
 
 def app():
     st.title("Wildfire Monitoring & Earth Engine API")
 
     st.markdown(
         """
-    This data platform provides access to live updates of satellite imagery, earth engine collections and dataBC geodatasets used in wildfire monitoring and natural resource assessments.
+    This data platform provides access to live updatessatellite imagery, earth engine collections and dataBC geodatasets used in wildfire monitoring and natural resource assessments.
 
     """
     )
